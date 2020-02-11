6c67b6d4de2c2977034fd39a44712eda0dddae36
Antony Chan <antony@truepic.com>
2020-02-11 15:37:27 -0800

    USE_ARMADILLO -> TJE_USE_ARMADILLO

--- tiny_jpeg.h
+++ tiny_jpeg.h
@@ -1101,7 +1101,6 @@ static int tjei_encode_main(TJEState* state,
     uint32_t bitbuffer = 0;
     uint32_t location = 0;
 
-#define USE_DIRECT_YUV
 #ifdef USE_RGB_888
     const arma::Mat<uint8_t> pixel ((uint8_t*) src_data, src_num_components, width*height, false, true);
 #elif !defined(USE_DIRECT_YUV)
@@ -1150,9 +1149,9 @@ const size_t padding = 0;
 
 #ifdef USE_RGB_888
                     du.row(off_y*8 + off_x) = arma::trans (RGB2YUV * pixel.col(row*width + col) + offset);
-#elif defined(USE_DIRECT_YUV) && defined(USE_ARMADILLO)
+#elif defined(USE_DIRECT_YUV) && defined(TJE_USE_ARMADILLO)
                     du.row(off_y*8 + off_x) = {Y(col, row), UV(col/2, row/2)>>8, UV(col/2, row/2) & 0xff};
-#elif defined(USE_DIRECT_YUV) && !defined(USE_ARMADILLO)
+#elif defined(USE_DIRECT_YUV) && !defined(TJE_USE_ARMADILLO)
                     du_y[off_y*8 + off_x] = Y[row*width + col];
                     du_b[off_y*8 + off_x] = UV[row*width/4 + col/2]>>8;
                     du_r[off_y*8 + off_x] = UV[row*width/4 + col/2] & 0xff;
